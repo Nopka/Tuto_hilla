ba4df1c15d20ca548c51af9b242998386291e93b
Hugo Roussillon <roussill1@univ-lorraine.fr>
2022-04-13 14:03:58 +0200

    problem on last 3 parts of 'forms and validation'

--- frontend/views/list/contact-form.ts
+++ frontend/views/list/contact-form.ts
@@ -5,21 +5,38 @@ import '@vaadin/button';
 import '@vaadin/combo-box';
 import '@vaadin/text-field';
 
+import {Binder, field} from '@hilla/form';
+import ContactModel from 'Frontend/generated/com/example/application/data/entity/ContactModel';
+import { crmStore } from 'Frontend/stores/app-store';
+import { listViewStore } from './list-view-store';
+
 @customElement('contact-form')
 export class ContactForm extends View {
+  protected binder = new Binder(this, ContactModel);
   render() {
+    const { model } = this.binder;
     return html`
-      <vaadin-text-field label="Prénom"></vaadin-text-field>
-      <vaadin-text-field label="Nom"></vaadin-text-field>
-      <vaadin-text-field label="Email"></vaadin-text-field>
-      <vaadin-combo-box label="Status"></vaadin-combo-box>
-      <vaadin-combo-box label="Entreprise"> </vaadin-combo-box>
+      <vaadin-text-field label="Prénom" ${field(model.firstName)}></vaadin-text-field>
+      <vaadin-text-field label="Nom" ${field(model.lastName)}></vaadin-text-field>
+      <vaadin-text-field label="Email" ${field(model.email)}></vaadin-text-field>
+      <vaadin-combo-box label="Status" ${field(model.status)} item-label-path="name" .items=${crmStore.statuses}></vaadin-combo-box>
+      <vaadin-combo-box label="Entreprise" ${field(model.company)}  item-label-path="name" .items=${crmStore.companies}> </vaadin-combo-box>
 
       <div class="flex gap-s">
         <vaadin-button theme="primary">Enregistrer</vaadin-button>
         <vaadin-button theme="error">Supprimer</vaadin-button>
-        <vaadin-button theme="tertiary">Annuler</vaadin-button>
+        <vaadin-button theme="tertiary" @click=${listViewStore.cancelEdit}>Annuler</vaadin-button>
       </div>
     `;
+  }
+  constructor() {
+    super();
+    this.autorun(() => {
+      if (listViewStore.selectedContact) {
+        this.binder.read(listViewStore.selectedContact);
+      } else {
+        this.binder.clear();
+      }
+    });
   }
 }
--- frontend/views/list/list-view.ts
+++ frontend/views/list/list-view.ts
@@ -5,30 +5,46 @@ import '@vaadin/text-field';
 import '@vaadin/button';
 import '@vaadin/grid';
 import '@vaadin/grid/vaadin-grid-column';
-import './contact-form'; // import of our custom component
+import './contact-form';
 
 import { crmStore } from 'Frontend/stores/app-store';
-
+import { listViewStore } from './list-view-store';
 
 @customElement('list-view')
 export class ListView extends View {
   render() {
     return html`
       <div class="toolbar flex gap-s">
-        <vaadin-text-field placeholder="Filtrer par nom" clear-button-visible></vaadin-text-field>
-        <vaadin-button>Ajouter un contact</vaadin-button>
+        <vaadin-text-field placeholder="Filtrer par nom" .value=${listViewStore.filterText} @input=${this.updateFilter} clear-button-visible></vaadin-text-field>
+        <vaadin-button @click=${listViewStore.editNew}>Ajouter un contact</vaadin-button>
       </div>
       <div class="content flex gap-m h-full">
-        <vaadin-grid class="grid h-full" .items=${crmStore.contacts}>
+        <vaadin-grid class="grid h-full" .items=${listViewStore.filteredContacts} .selectedItems=${[listViewStore.selectedContact]} @active-item-changed=${this.handleGridSelection}>
           <vaadin-grid-column path="firstName" header="Prénom" auto-width> </vaadin-grid-column>
           <vaadin-grid-column path="lastName" header="Nom" auto-width> </vaadin-grid-column>
           <vaadin-grid-column path="email" header="Email" auto-width> </vaadin-grid-column>
           <vaadin-grid-column path="status.name" header="Status" auto-width></vaadin-grid-column>
           <vaadin-grid-column path="company.name" header="Entreprise" auto-width></vaadin-grid-column>
         </vaadin-grid>
-        <contact-form class="flex flex-col gap-s"></contact-form>
+        <contact-form
+          class="flex flex-col gap-s"
+          ?hidden=${!listViewStore.selectedContact}>
+        </contact-form>
       </div>
     `;
+  }
+
+  updateFilter(e: { target: HTMLInputElement }) {
+    listViewStore.updateFilter(e.target.value);
+  }
+
+  firstSelectionEvent = true;
+  handleGridSelection(e: CustomEvent){
+    if (this.firstSelectionEvent) {
+      this.firstSelectionEvent = false;
+      return;
+    }
+    listViewStore.setSelectedContact(e.detail.value);
   }
 
   connectedCallback() {
@@ -42,5 +58,13 @@ export class ListView extends View {
       'w-full',
       'h-full'
     );
+    this.autorun(() => {
+      if (listViewStore.selectedContact) {
+        this.classList.add("editing");
+      } else {
+        this.classList.remove("editing");
+      }
+
+    });
   }
 }
